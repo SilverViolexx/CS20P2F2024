32ba8ccfae0e034c2cfad8ab0de3e6d860ae6519
38207518 <38207518@L815WD220033.edu.cbe.ab.ca>
2024-09-11 11:00:48 -0600

    11:00AM

---
+++ Chapter3/src/SkillBuilder/Digits.java
@@ -0,0 +1,11 @@
+package SkillBuilder;
+
+public class Digits {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+
+}
---
+++ Chapter3/src/SkillBuilder/RetanglePerimeter.java
@@ -0,0 +1,28 @@
+package SkillBuilder;
+
+import java.util.*;
+
+public class RetanglePerimeter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	
+	Scanner userInput = new Scanner(System.in);
+	
+	//Declaration &  initialization
+	    System.out.print("Please enter the length in:");
+		int length = userInput.nextInt();
+		
+		System.out.print("Please enter the width:");
+		int width = userInput.nextInt();;
+		
+		int area = length * width;
+		
+		System.out.print("The area of the rectangle is: " + area);
+		
+		
+		
+		
+	}
+}
