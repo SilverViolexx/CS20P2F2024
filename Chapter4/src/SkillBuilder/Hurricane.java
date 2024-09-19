3b2b91b4086062748bc51dac326e9a62b6c0a7ea
38207518 <38207518@L815WD220033.edu.cbe.ab.ca>
2024-09-19 10:23:51 -0600

    10:23AM

--- Chapter4/src/SkillBuilder/Hurricane.java
+++ Chapter4/src/SkillBuilder/Hurricane.java
@@ -1,5 +1,23 @@
+/*
+
+Program: ProjectMastery.java          Last Date of this Revision: September 15, 2024
+
+Purpose: Create a Hurricane application that displays the wind speed for the hurricane category entered 
+by the user. 
+
+Author: Amanda Ly
+School: CHHS
+Course: Computer Programming 20
+ 
+
+*/
 package SkillBuilder;
 
 public class Hurricane {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
