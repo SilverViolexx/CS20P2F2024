32723255474811f3d274f97c1ebe56d5778d55a1
38207518 <38207518@L815WD220033.edu.cbe.ab.ca>
2024-09-20 10:49:04 -0600

    10:48AM

--- Chapter4/src/SkillBuilder/Hurricane.java
+++ Chapter4/src/SkillBuilder/Hurricane.java
@@ -13,11 +13,58 @@ Course: Computer Programming 20
 */
 package SkillBuilder;
 
+import java.util.Scanner;
+
 public class Hurricane {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+	//Declaration Area
+	int category;
+		
+	//Preparing for user input
+	Scanner userInput = new Scanner(System.in);
+			
+	//Prompt the user and initialize the variables
+	System.out.print("Please enter the huricane category: ");
+	category = userInput.nextInt();
+	
+	if (category == 1) {
+		System.out.print("Miles per hour: 74-95 mph"
+				+ "\nKnots: 64-82 kt"
+				+ "\nKilometers per hour: 119-153 km/hr");
+	}
+		
+	if (category == 2) {
+		System.out.print("Miles per hour: 96-110 mph"
+				+ "\nKnots: 83-95 kt"
+				+ "\nKilometers per hour: 154-177 km/hr");
+	}
+	
+	if (category == 3) {
+		System.out.print("Miles per hour: 111-130 mph"
+				+ "\nKnots: 96-113 kt"
+				+ "\nKilometers per hour: 178-209 km/hr");
+	}
+	
+	if (category == 4) {
+		System.out.print("Miles per hour: 131-155 mph"
+				+ "\nKnots: 114-135 kt"
+				+ "\nKilometers per hour: 210-249 km/hr");
+	}
+	
+	if (category == 5) {
+		System.out.print("Miles per hour: >155 mph"
+				+ "\nKnots: >135 kt"
+				+ "\nKilometers per hour: >249 km/hr");
+	}
+	
+	else {
+		System.out.print("Invalid category, please try again.");
+	}
+	
+	
+	
 	}
 
 }
